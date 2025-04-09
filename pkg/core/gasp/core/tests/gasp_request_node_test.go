b0c362ea1a24e3a071f4ad9983a479c0b657072a
Augustyn Chmiel <augustyn.chmiel@acoustic.com>
2025-04-09 11:56:05 +0200

    adding gasp request node unit tests

---
+++ pkg/core/gasp/core/tests/gasp_request_node_test.go
@@ -0,0 +1,53 @@
+package core__test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/bsv-blockchain/go-sdk/overlay"
+	"github.com/stretchr/testify/require"
+
+	"github.com/4chain-ag/go-overlay-services/pkg/core/gasp/core"
+)
+
+
+func TestGASP_RequestNode_ShouldReturnNode(t *testing.T) {
+	// given
+	ctx := context.Background()
+	expectedNode := &core.GASPNode{}
+	gasp := &core.GASP{
+		Storage: fakeStorage{
+			hydrateGASPNodeFunc: func(ctx context.Context, graphID, outpoint *overlay.Outpoint, metadata bool) (*core.GASPNode, error) {
+				return expectedNode, nil
+			},
+		},
+	}
+
+	// when
+	node, err := gasp.RequestNode(ctx, &overlay.Outpoint{}, &overlay.Outpoint{}, true)
+
+	// then
+	require.NoError(t, err)
+	require.Equal(t, expectedNode, node)
+}
+
+func TestGASP_RequestNode_ShouldReturnError(t *testing.T) {
+	// given
+	ctx := context.Background()
+	gasp := &core.GASP{
+		Storage: fakeStorage{
+			hydrateGASPNodeFunc: func(ctx context.Context, graphID, outpoint *overlay.Outpoint, metadata bool) (*core.GASPNode, error) {
+				return nil, errors.New("hydrate-fail")
+			},
+		},
+	}
+
+	// when
+	node, err := gasp.RequestNode(ctx, &overlay.Outpoint{}, &overlay.Outpoint{}, true)
+
+	// then
+	require.Error(t, err)
+	require.Nil(t, node)
+	require.EqualError(t, err, "hydrate-fail")
+}
---
+++ pkg/core/gasp/core/tests/gasp_utils_test.go
@@ -0,0 +1,41 @@
+package core__test
+
+import (
+	"context"
+	"errors"
+
+	"github.com/bsv-blockchain/go-sdk/chainhash"
+	"github.com/bsv-blockchain/go-sdk/overlay"
+
+	"github.com/4chain-ag/go-overlay-services/pkg/core/gasp/core"
+)
+
+type fakeStorage struct {
+	hydrateGASPNodeFunc func(ctx context.Context, graphID, outpoint *overlay.Outpoint, metadata bool) (*core.GASPNode, error)
+}
+
+func (f fakeStorage) HydrateGASPNode(ctx context.Context, graphID, outpoint *overlay.Outpoint, metadata bool) (*core.GASPNode, error) {
+	if f.hydrateGASPNodeFunc != nil {
+		return f.hydrateGASPNodeFunc(ctx, graphID, outpoint, metadata)
+	}
+	return nil, errors.New("not implemented")
+}
+
+func (f fakeStorage) FindKnownUTXOs(ctx context.Context, since uint32) ([]*overlay.Outpoint, error) { 
+	return nil, nil 
+}
+func (f fakeStorage) FindNeededInputs(ctx context.Context, tx *core.GASPNode) (*core.GASPNodeResponse, error) { 
+	return nil, nil 
+}
+func (f fakeStorage) AppendToGraph(ctx context.Context, tx *core.GASPNode, spentBy *chainhash.Hash) error { 
+	return nil 
+}
+func (f fakeStorage) ValidateGraphAnchor(ctx context.Context, graphID *overlay.Outpoint) error { 
+	return nil 
+}
+func (f fakeStorage) DiscardGraph(ctx context.Context, graphID *overlay.Outpoint) error { 
+	return nil 
+}
+func (f fakeStorage) FinalizeGraph(ctx context.Context, graphID *overlay.Outpoint) error { 
+	return nil 
+}
