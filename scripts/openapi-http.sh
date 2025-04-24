434697f00d66b1e6e996cedf2e1e437d93b5365a
mgosek-4chain <michal.gosek@4chain.studio>
2025-04-24 14:43:39 +0200

    refactor(#97): move docs dir to the root level.

--- scripts/openapi-http.sh
+++ scripts/openapi-http.sh
@@ -7,4 +7,4 @@ oapi-codegen -config ../api/openapi/paths/admin/response-models-cfg.yaml ../api/
 oapi-codegen -config ../api/openapi/paths/non_admin/response-models-cfg.yaml ../api/openapi/paths/non_admin/response-models.yaml
 oapi-codegen -config ../api/openapi/paths/non_admin/request-models-cfg.yaml ../api/openapi/paths/non_admin/request-models.yaml
 
-swagger-cli bundle ../api/openapi/server/api.yaml -o docs/api/openapi/bundled.yaml -t yaml
+swagger-cli bundle ../api/openapi/server/api.yaml -o ../docs/api/openapi/bundled.yaml -t yaml
