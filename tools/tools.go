e2e142b2e64a06cf5425a38cd2761fd81439a70c
mgosek-4chain <michal.gosek@4chain.studio>
2025-05-12 10:53:53 +0200

    refactor(#97): build fix.

--- tools/tools.go
+++ tools/tools.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 //go:generate go get -tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest
 //go:generate go tool oapi-codegen --config=../api/openapi/server/api-cfg.yaml         ../api/openapi/server/api.yaml
 //go:generate go tool oapi-codegen --config=../api/openapi/paths/admin/responses-cfg.yaml ../api/openapi/paths/admin/responses.yaml
 //go:generate go tool oapi-codegen --config=../api/openapi/paths/non_admin/responses-cfg.yaml ../api/openapi/paths/non_admin/responses.yaml
 //go:generate go tool oapi-codegen --config=../api/openapi/paths/non_admin/request-bodies-cfg.yaml ../api/openapi/paths/non_admin/request-bodies.yaml
+
+func main() {
+	fmt.Println("Overlay HTTP API code generated")
+}
