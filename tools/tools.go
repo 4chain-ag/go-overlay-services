4fd45e0c414255aee9a2ffa9103bb431ae0e9c3a
mgosek-4chain <michal.gosek@4chain.studio>
2025-05-14 14:21:45 +0200

    fix(#97): add missing main func.

--- tools/tools.go
+++ tools/tools.go
@@ -4,3 +4,5 @@ package main
 //go:generate go tool oapi-codegen --config=../api/openapi/paths/admin/responses-cfg.yaml ../api/openapi/paths/admin/responses.yaml
 //go:generate go tool oapi-codegen --config=../api/openapi/paths/non_admin/responses-cfg.yaml ../api/openapi/paths/non_admin/responses.yaml
 //go:generate go tool oapi-codegen --config=../api/openapi/paths/non_admin/request-bodies-cfg.yaml ../api/openapi/paths/non_admin/request-bodies.yaml
+
+func main() {}
