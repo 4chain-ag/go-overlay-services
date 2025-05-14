c44dc445dbf314cd5db8f39cc4e5a59cd3ad2223
mgosek-4chain <michal.gosek@4chain.studio>
2025-05-14 14:08:25 +0200

    refactor(#97): remove unecessary middleware loop in the constructor func. Restore tools pkg, to simplify view of the generated code.

---
+++ tools/tools.go
@@ -0,0 +1,6 @@
+package main
+
+//go:generate go tool oapi-codegen --config=../api/openapi/server/api-cfg.yaml         ../api/openapi/server/api.yaml
+//go:generate go tool oapi-codegen --config=../api/openapi/paths/admin/responses-cfg.yaml ../api/openapi/paths/admin/responses.yaml
+//go:generate go tool oapi-codegen --config=../api/openapi/paths/non_admin/responses-cfg.yaml ../api/openapi/paths/non_admin/responses.yaml
+//go:generate go tool oapi-codegen --config=../api/openapi/paths/non_admin/request-bodies-cfg.yaml ../api/openapi/paths/non_admin/request-bodies.yaml
