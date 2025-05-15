1dae71b673d80eb49746c34dd41ae0c3d7c9ef11
mgosek-4chain <michal.gosek@4chain.studio>
2025-05-15 08:39:21 +0200

    fix(#97): correct directories path to the OpenAPI spec files, add srv2 package as placeholder for the HTTP v2 definitions, restored generated code base.

--- tools/tools.go
+++
@@ -1,8 +0,0 @@
-package main
-
-//go:generate go tool oapi-codegen --config=../api/openapi/server/api-cfg.yaml         ../api/openapi/server/api.yaml
-//go:generate go tool oapi-codegen --config=../api/openapi/paths/admin/responses-cfg.yaml ../api/openapi/paths/admin/responses.yaml
-//go:generate go tool oapi-codegen --config=../api/openapi/paths/non_admin/responses-cfg.yaml ../api/openapi/paths/non_admin/responses.yaml
-//go:generate go tool oapi-codegen --config=../api/openapi/paths/non_admin/request-bodies-cfg.yaml ../api/openapi/paths/non_admin/request-bodies.yaml
-
-func main() {}
