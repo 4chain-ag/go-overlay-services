b0294b4e764d0066a7829f7b686b8f3d997c2101
cain4chain <cain.nussdorfer@4chain.studio>
2025-05-16 21:04:26 +1000

    fix(#130): lint

--- pkg/server2/internal/app/request_foreign_gasp_node_service_test.go
+++ pkg/server2/internal/app/request_foreign_gasp_node_service_test.go
@@ -13,195 +13,116 @@ import (
 )
 
 func TestRequestForeignGASPNodeService_ProviderFailure(t *testing.T) {
-
 	// given:
-
 	expectations := testabilities.RequestForeignGASPNodeProviderMockExpectations{
-
 		ProvideForeignGASPNodeCall: true,
-
-		Error: errors.New("internal request foreign GASP node service test error"),
+		Error:                      errors.New("internal request foreign GASP node service test error"),
 	}
-
 	mock := testabilities.NewRequestForeignGASPNodeProviderMock(t, expectations)
-
 	service, err := app.NewRequestForeignGASPNodeService(mock)
-
 	require.NoError(t, err)
-
 	graphID := &overlay.Outpoint{}
-
 	outpoint := &overlay.Outpoint{}
-
 	topic := "test-topic"
 
 	// when:
-
 	node, err := service.RequestForeignGASPNode(context.Background(), graphID, outpoint, topic)
 
 	// then:
-
 	var appErr app.Error
-
 	require.ErrorAs(t, err, &appErr)
-
 	require.Equal(t, app.ErrorTypeProviderFailure, appErr.ErrorType())
-
 	require.Nil(t, node)
-
 	mock.AssertCalled()
-
 }
 
 func TestRequestForeignGASPNodeService_MissingTopic(t *testing.T) {
-
 	// given:
-
 	mock := testabilities.NewRequestForeignGASPNodeProviderMock(t, testabilities.RequestForeignGASPNodeProviderMockExpectations{})
-
 	service, err := app.NewRequestForeignGASPNodeService(mock)
-
 	require.NoError(t, err)
-
 	graphID := &overlay.Outpoint{}
-
 	outpoint := &overlay.Outpoint{}
-
 	topic := ""
 
 	// when:
-
 	node, err := service.RequestForeignGASPNode(context.Background(), graphID, outpoint, topic)
 
 	// then:
-
 	var appErr app.Error
-
 	require.ErrorAs(t, err, &appErr)
-
 	require.Equal(t, app.ErrorTypeIncorrectInput, appErr.ErrorType())
-
 	require.Nil(t, node)
-
 }
 
 func TestRequestForeignGASPNodeService_MissingGraphID(t *testing.T) {
-
 	// given:
-
 	mock := testabilities.NewRequestForeignGASPNodeProviderMock(t, testabilities.RequestForeignGASPNodeProviderMockExpectations{})
-
 	service, err := app.NewRequestForeignGASPNodeService(mock)
-
 	require.NoError(t, err)
-
 	outpoint := &overlay.Outpoint{}
-
 	topic := "test-topic"
 
 	// when:
-
 	node, err := service.RequestForeignGASPNode(context.Background(), nil, outpoint, topic)
 
 	// then:
-
 	var appErr app.Error
-
 	require.ErrorAs(t, err, &appErr)
-
 	require.Equal(t, app.ErrorTypeIncorrectInput, appErr.ErrorType())
-
 	require.Nil(t, node)
-
 }
 
 func TestRequestForeignGASPNodeService_MissingOutpoint(t *testing.T) {
-
 	// given:
-
 	mock := testabilities.NewRequestForeignGASPNodeProviderMock(t, testabilities.RequestForeignGASPNodeProviderMockExpectations{})
-
 	service, err := app.NewRequestForeignGASPNodeService(mock)
-
 	require.NoError(t, err)
-
 	graphID := &overlay.Outpoint{}
-
 	topic := "test-topic"
 
 	// when:
-
 	node, err := service.RequestForeignGASPNode(context.Background(), graphID, nil, topic)
 
 	// then:
-
 	var appErr app.Error
-
 	require.ErrorAs(t, err, &appErr)
-
 	require.Equal(t, app.ErrorTypeIncorrectInput, appErr.ErrorType())
-
 	require.Nil(t, node)
-
 }
 
 func TestRequestForeignGASPNodeService_NilProvider(t *testing.T) {
-
 	// given/when:
-
 	service, err := app.NewRequestForeignGASPNodeService(nil)
 
 	// then:
-
 	require.Error(t, err)
-
 	var appErr app.Error
-
 	require.ErrorAs(t, err, &appErr)
-
 	require.Equal(t, app.ErrorTypeIncorrectInput, appErr.ErrorType())
-
 	require.Nil(t, service)
-
 }
 
 func TestRequestForeignGASPNodeService_ValidCase(t *testing.T) {
-
 	// given:
-
 	expectedNode := &core.GASPNode{}
-
 	expectations := testabilities.RequestForeignGASPNodeProviderMockExpectations{
-
 		ProvideForeignGASPNodeCall: true,
-
-		Node: expectedNode,
-
-		Error: nil,
+		Node:                       expectedNode,
+		Error:                      nil,
 	}
-
 	mock := testabilities.NewRequestForeignGASPNodeProviderMock(t, expectations)
-
 	service, err := app.NewRequestForeignGASPNodeService(mock)
-
 	require.NoError(t, err)
-
 	graphID := &overlay.Outpoint{}
-
 	outpoint := &overlay.Outpoint{}
-
 	topic := "test-topic"
 
 	// when:
-
 	node, err := service.RequestForeignGASPNode(context.Background(), graphID, outpoint, topic)
 
 	// then:
-
 	require.NoError(t, err)
-
 	require.Equal(t, expectedNode, node)
-
 	mock.AssertCalled()
-
 }
