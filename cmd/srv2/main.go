3407084cbeb5812331e5f18acb97b1515698e044
mgosek-4chain <michal.gosek@4chain.studio>
2025-05-20 07:56:02 +0200

    feat(#97):  add support to import or export configuration for the http server.

--- cmd/srv2/main.go
+++ cmd/srv2/main.go
@@ -1,9 +1,60 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/4chain-ag/go-overlay-services/pkg/server2"
+	"github.com/4chain-ag/go-overlay-services/pkg/server2/config"
+	"github.com/4chain-ag/go-overlay-services/pkg/server2/config/loaders"
+)
+
 //go:generate go tool oapi-codegen --config=../../api/openapi/server/api-cfg.yaml         ../../api/openapi/server/api.yaml
 //go:generate go tool oapi-codegen --config=../../api/openapi/paths/admin/responses-cfg.yaml ../../api/openapi/paths/admin/responses.yaml
 //go:generate go tool oapi-codegen --config=../../api/openapi/paths/non_admin/responses-cfg.yaml ../../api/openapi/paths/non_admin/responses.yaml
 //go:generate go tool oapi-codegen --config=../../api/openapi/paths/non_admin/request-bodies-cfg.yaml ../../api/openapi/paths/non_admin/request-bodies.yaml
 func main() {
-	// This code section will be updated in upcoming PRs for the HTTP v2.
+	if err := execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func execute() error {
+	configPath := flag.String("config", loaders.DefaultConfigFilePath, "Path to the configuration file")
+	flag.Parse()
+
+	cfg, err := config.LoadFromPath(*configPath, "OVERLAY")
+	if err != nil {
+		return fmt.Errorf("load config op failed: %w", err)
+	}
+
+	ctx := context.Background()
+	srv := server2.New(server2.WithConfig(cfg))
+	done := make(chan struct{})
+
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+
+		// We received an interrupt signal, shut down.
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("http server shutdown err: %v", err)
+		}
+		close(done)
+	}()
+
+	err = srv.ListenAndServe(ctx)
+	if !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("http server listen and serve op failure: %w", err)
+	}
+	<-done
+
+	return nil
 }
