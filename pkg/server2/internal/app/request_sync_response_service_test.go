9c772d10da4b03ca4b331eb3698fbd498dd1d0bf
cain4chain <cain.nussdorfer@4chain.studio>
2025-05-26 11:46:52 +1000

    feat(#131): add application code for request sync response endpoint, service level testibilities

---
+++ pkg/server2/internal/app/request_sync_response_service_test.go
@@ -0,0 +1,85 @@
+package app_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/4chain-ag/go-overlay-services/pkg/core/gasp/core"
+	"github.com/4chain-ag/go-overlay-services/pkg/server2/internal/app"
+	"github.com/4chain-ag/go-overlay-services/pkg/server2/internal/testabilities"
+	"github.com/bsv-blockchain/go-sdk/overlay"
+	"github.com/stretchr/testify/require"
+)
+
+
+func TestRequestSyncResponseService_ValidCase(t *testing.T) {
+	// given:
+	expectedResponse := &core.GASPInitialResponse{UTXOList: []*overlay.Outpoint{{}}, Since: 1000}
+
+	provider := testabilities.NewRequestSyncResponseProviderMock(t, testabilities.RequestSyncResponseProviderMockExpectations{ProvideForeignSyncResponseCall: true, Response: expectedResponse})
+
+	service := app.NewRequestSyncResponseService(provider)
+
+	// when:
+	response, err := service.RequestSyncResponse(context.Background(), &core.GASPInitialRequest{Version: 1,Since:   500}, "test-topic")
+
+	// then:
+	require.NoError(t, err)
+	require.Equal(t, expectedResponse, response)
+	provider.AssertCalled()
+}
+
+
+func TestRequestSyncResponseService_InvalidCases(t *testing.T) {
+	tests := map[string]struct {
+		expectedError app.Error
+		expectations  testabilities.RequestSyncResponseProviderMockExpectations
+		topic         string
+		initialRequest *core.GASPInitialRequest
+	}{
+		"Request sync response service fails to handle request - empty topic": {
+			topic: "",
+			expectations: testabilities.RequestSyncResponseProviderMockExpectations{
+				ProvideForeignSyncResponseCall: false,
+			},
+			initialRequest: &core.GASPInitialRequest{
+				Version: 1,
+				Since:   500,
+			},
+			expectedError: app.NewRequestSyncResponseInvalidInputError(),
+		},
+		"Request sync response service fails to handle request - provider error": {
+			topic: "test-topic",
+			expectations: testabilities.RequestSyncResponseProviderMockExpectations{
+				ProvideForeignSyncResponseCall: true,
+				Error:                          errors.New("provider error"),
+			},
+			initialRequest: &core.GASPInitialRequest{
+				Version: 1,
+				Since:   500,
+			},
+			expectedError: app.NewRequestSyncResponseProviderError(errors.New("provider error")),
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			// given:
+			mock := testabilities.NewRequestSyncResponseProviderMock(t, tc.expectations)
+			service := app.NewRequestSyncResponseService(mock)
+
+			// when:
+			document, err := service.RequestSyncResponse(context.Background(), tc.initialRequest, tc.topic)
+
+			// then:
+			var actualErr app.Error
+			require.ErrorAs(t, err, &actualErr)
+			require.Equal(t, tc.expectedError, actualErr)
+
+			require.Empty(t, document)
+			mock.AssertCalled()
+		})
+	}
+
+}
---
+++ pkg/server2/internal/testabilities/request_sync_response_provider_mock.go
@@ -0,0 +1,55 @@
+package testabilities
+
+import (
+	"context"
+	"testing"
+
+	"github.com/4chain-ag/go-overlay-services/pkg/core/gasp/core"
+	"github.com/stretchr/testify/require"
+)
+
+// RequestSyncResponseProviderMockExpectations defines mock expectations.
+type RequestSyncResponseProviderMockExpectations struct {
+	Error error
+	Response *core.GASPInitialResponse
+	ProvideForeignSyncResponseCall bool
+}
+
+var DefaultRequestSyncResponseProviderMockExpectations = RequestSyncResponseProviderMockExpectations{
+	Error: nil,
+	Response: &core.GASPInitialResponse{},
+	ProvideForeignSyncResponseCall: true,
+}
+
+// RequestSyncResponseProviderMock is a mock provider.
+type RequestSyncResponseProviderMock struct {
+	t            *testing.T
+	expectations RequestSyncResponseProviderMockExpectations
+	requestSyncResponseCall       bool
+}
+
+// ProvideForeignSyncResponse mocks the method.
+func (m *RequestSyncResponseProviderMock) ProvideForeignSyncResponse(ctx context.Context, initialRequest *core.GASPInitialRequest, topic string) (*core.GASPInitialResponse, error) {
+	m.t.Helper()
+	m.requestSyncResponseCall = true
+
+	if m.expectations.Error != nil {
+		return nil, m.expectations.Error
+	}
+
+	return m.expectations.Response, nil
+}
+
+// AssertCalled verifies the method was called as expected.
+func (m *RequestSyncResponseProviderMock) AssertCalled() {
+	m.t.Helper()
+	require.Equal(m.t, m.expectations.ProvideForeignSyncResponseCall, m.requestSyncResponseCall, "Discrepancy between expected and actual ProvideForeignSyncResponseCall")
+}
+
+// NewRequestSyncResponseProviderMock creates a new mock provider.
+func NewRequestSyncResponseProviderMock(t *testing.T, expectations RequestSyncResponseProviderMockExpectations) *RequestSyncResponseProviderMock {
+	return &RequestSyncResponseProviderMock{
+		t:            t,
+		expectations: expectations,
+	}
+}
